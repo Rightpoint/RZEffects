1cd29dcdd7477e7086358cdf22fb72b2515fb2e7
John Stricker <john.stricker@raizlabs.com>
2015-03-19 10:48:40 -0400

    starting on scene demo

---
+++ Example/RZSceneDemo/RZSceneDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RZSceneDemo
+//
+//  Created by John Stricker on 3/19/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
