1fb6fbd84c779385cfd5e8ffb5e9ec695dd8f9be
John Stricker <john.stricker@raizlabs.com>
2015-04-10 20:57:51 -0700

    no message

---
+++ Example/RZXSceneDemo/RZXSceneDemo/RZXNode.h
@@ -0,0 +1,13 @@
+//
+//  RZXNode.h
+//  RZXSceneDemo
+//
+//  Created by John Stricker on 3/26/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RZXNode : NSObject
+
+@end
---
+++ Example/RZXSceneDemo/RZXSceneDemo/RZXNode.m
@@ -0,0 +1,13 @@
+//
+//  RZXNode.m
+//  RZXSceneDemo
+//
+//  Created by John Stricker on 3/26/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import "RZXNode.h"
+
+@implementation RZXNode
+
+@end
